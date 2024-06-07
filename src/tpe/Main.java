c0c86f3f4f5ec7f01c722e39114b240110db1b70
ebrites <emabrio@gmail.com>
2024-06-06 21:30:19 -0300

    Corrije tiempo de solucion greedy

--- src/tpe/Main.java
+++ src/tpe/Main.java
@@ -6,19 +6,9 @@ public class Main {
 
 	public static void main(String args[]) {
 		Servicios servicios = new Servicios("src/tpe/datasets/Procesadores.csv", "src/tpe/datasets/Tareas.csv");
-	//	servicios.asginarTareasConBacktracking(50);
-		//servicios.reiniciarProcesadores();
+		servicios.asginarTareasConBacktracking(50);
+		servicios.reiniciarProcesadores();
 		Solucion solucionGreedy = servicios.asignarTareasConGreedy(50);
 		System.out.println(solucionGreedy);
-
-	/*Tarea tareaId =  servicios.servicio1("T3");
-	System.out.println("Tarea por id: " + tareaId.toString());
-
-	List<Tarea> tareasCriticas =  servicios.servicio2(true);
-		System.out.println("Tareas criticas:" + tareasCriticas.toString());
-	List<Tarea> tareasNoCriticas =  servicios.servicio2(false);
-		System.out.println("Tareas no criticas" + tareasNoCriticas.toString());
-	List<Tarea> tareasPorNivel =  servicios.servicio3(1,65);
-		System.out.println("Tareas entre niveles" + tareasPorNivel.toString());*/
 	}
 }
